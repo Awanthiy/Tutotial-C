e2e3cf85dbfd6298a12c5ca97def6f199cdbe2fd
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2023-06-15 19:32:04 +0530

    Create Q 2.c

---
+++ practicle 1/Q 2.c
@@ -0,0 +1,18 @@
+/*2.	Display the following output using printf() statements
+*
+**
+***
+****
+*****
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+    return 0;
+}
