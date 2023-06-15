939a4565d54553ed886e1b5455341a5b41ca19b0
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2023-06-15 19:31:21 +0530

    Create Q 1.c

---
+++ practicle 1/Q 1.c
@@ -0,0 +1,11 @@
+//1.	Display your name and school name in two separate lines
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    printf("My Name is Awanthi. \n");
+    printf("My School is ST/Agnes Girls school.\n");
+    return 0;
+}
