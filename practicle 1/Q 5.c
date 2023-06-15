7d5a5527e5ed72434a78e1734d25256797855c24
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2023-06-15 19:34:22 +0530

    Create Q 5.c

---
+++ practicle 1/Q 5.c
@@ -0,0 +1,20 @@
+//5.	Input two numbers with decimals and display the average with decimals
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a,b,avg;
+
+    printf("Enter first no: ");
+    scanf("%f",&a);
+
+    printf("Enter second no: ");
+    scanf("%f",&b);
+
+    avg=(a+b)/2;
+    printf("Average is %f",avg);
+    scanf("%f=(%F+%f)/2");
+
+
+}
