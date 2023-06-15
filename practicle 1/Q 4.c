801a634b156fdcf71323a7d561404d25a50bcc7c
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2023-06-15 19:33:44 +0530

    Create Q 4.c

---
+++ practicle 1/Q 4.c
@@ -0,0 +1,17 @@
+//4.	Input two integers and display the total
+#include <stdio.h>
+#include <stdlib.h>
+ int main()
+{
+    int a,b,Sum;
+
+    printf("Enter first number: ");
+    scanf("%d",&a);
+
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    Sum=a+b;
+    printf("Sum is %d\n",Sum);
+    scanf("%d=%d+%d");
+    return 0;
+}
