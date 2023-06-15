23dfe6ece2272f7ecd35256ebe758b6fb489e385
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2023-06-15 19:32:59 +0530

    Create Q 3.c

---
+++ practicle 1/Q 3.c
@@ -0,0 +1,24 @@
+//3.	Input values for int,float,double and char data types and display the value of each of the variable.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    float b;
+    double c;
+    char d;
+
+    printf("Enter a positive number : \n");
+    scanf("%d",&a);
+
+    printf("Enter a Decimal Number : \n");
+    scanf("%f",&b);
+
+    printf("Enter a Float");
+    scanf("%Enter a Decimal Number : \n");
+
+    printf("Enter a character")
+    return 0;
+}
